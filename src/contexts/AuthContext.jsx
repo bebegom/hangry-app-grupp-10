e3723ad349b89601c27812b0fcc4323133fa16fe
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-09-22 20:35:14 +0200

    added auth context

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useContext } from 'react'
+import { auth } from '../firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+
+const AuthContext = createContext()
+
+const useAuthContext = () => {
+    return useContext(AuthContext)
+}
+
+const AuthContextProvider = ({ children }) => {
+    const login = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const values = {
+        // everything the children needs
+        login,
+    }
+
+    return (
+        <AuthContext.Provider value={values}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export {
+    AuthContextProvider as default,
+    useAuthContext,
+}
