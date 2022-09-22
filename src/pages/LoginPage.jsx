e04abdb945056e3444fa248e34dbd0566ca99dfd
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-09-22 20:35:33 +0200

    added login page and moved the logic to a component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,48 +1,10 @@
-import React, { useRef, useState } from 'react'
-import { Alert, Button, Card, Col, Container, Form, Row  } from 'react-bootstrap'
+import React from 'react'
+import LoginForm from "../components/LoginForm"
 
-const LoginPage = () => {
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        setError(null);
-    }
-
+const LogInPage = () => {
     return (
-        <>
-            <Container>
-                <Row>
-                    <Col>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Log In</Card.Title>
-
-                                {error && (<Alert variant='warning'>{error}</Alert>)}
-
-                                <Form onSubmit={handleSubmit}>
-                                    <Form.Group id="email">
-                                        <Form.Label>Email</Form.Label>
-                                        <Form.Control type="email" ref={emailRef} required />
-                                    </Form.Group>
-
-                                    <Form.Group id="password">
-                                        <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" ref={passwordRef} required />
-                                    </Form.Group>
-
-                                    <Button disabled={loading} type="submit">Log In</Button>
-                                </Form>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </>
+        <LoginForm />
     )
 }
 
-export default LoginPage
+export default LogInPage
