9a9b782fd4fc15a6b15c64d54b881a4fd69c3700
TirapatS <tirapat.s02@gmail.com>
2022-09-22 12:38:28 +0200

    map now works

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,20 @@
-
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
+import '../assets/scss/mapStyling.scss'
 
 function HomePage() {
+
   return (
-    <div>HomePage</div>
+    <LoadScript googleMapsApiKey= {`${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`} >
+        <GoogleMap
+            zoom={10}
+            center={{
+              lat: -3.745,
+              lng: -38.523
+            }}
+            mapContainerClassName="mapContainer"
+        >
+        </GoogleMap>
+    </LoadScript>
   )
 }
 
