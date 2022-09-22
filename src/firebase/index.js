b51ad556da98299fb05a4ae45063510e71f7dfc7
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-09-22 15:48:05 +0200

    added auth instance to app

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
@@ -14,6 +15,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
+// Firebase Auth instance 
+const auth = getAuth(app)
+
 // Firestore instance
 const db = getFirestore(app)
 
@@ -22,6 +26,7 @@ const storage = getStorage(app)
 
 export {
     app as default,
+	auth,
     db,
     storage,
 }
