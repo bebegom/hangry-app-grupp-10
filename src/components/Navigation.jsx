96d75fd13db3aeaa5148837d56d8460134f798e6
TirapatS <tirapat.s02@gmail.com>
2022-09-22 20:47:49 +0200

    changed the structure of the code

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
-function Navigation() {
+const Navigation = () => {
     
     return (
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,12 @@
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
-import '../assets/scss/mapStyling.scss'
-import Navigation from '../components/Navigation'
+import GoogleMapsApi from '../components/GoogleMapsApi'
 
-function HomePage() {
-    const { isLoaded, loadError } = useJsApiLoader({
-        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-    })
 
-    if(isLoaded) {
-        
-        return (
-            <GoogleMap
-                    zoom={12}
-                    center={{
-                      lat: 55.60,
-                      lng: 13
-                    }}
-                    mapContainerClassName="mapContainer"
-                >
-            </GoogleMap>
-          )
-    }
+const HomePage = () => {
+   return (
+
+    <GoogleMapsApi/>
+    
+   )
 }
 
 export default HomePage
