665202f5aa40514eff808b3c8d53ab16124c4455
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-09-22 13:16:08 +0200

    using register from react-hook-form and showing the data in the console when submitting

--- src/pages/CreateNewPlacePage.jsx
+++ src/pages/CreateNewPlacePage.jsx
@@ -3,39 +3,44 @@ import {useForm} from 'react-hook-form'
 import { Container, Form, Button } from 'react-bootstrap'
 
 const CreateNewPlacePage = () => {
-    const {handleSubmit} = useForm()
+    const {handleSubmit, register} = useForm()
+
+    const onCreatePlace = (data) => {
+        console.log(data)
+    }
+
     return (
         <Container>
             <h1>Create a new place for the hangry</h1>
-            <Form noValidate>
+            <Form noValidate onSubmit={handleSubmit(onCreatePlace)}>
                 <Form.Group className='mb-3' controlId='namn'>
                     <Form.Label>Namn</Form.Label>
-                    <Form.Control type='text' />
+                    <Form.Control {...register('name')} type='text' />
                 </Form.Group>
 
                 <Form.Group className='mb-3' controlId='adress'>
                     <Form.Label>Adress</Form.Label>
-                    <Form.Control type='text' />
+                    <Form.Control {...register('adress')} type='text' />
                 </Form.Group>
 
                 <Form.Group className='mb-3' controlId='ort'>
                     <Form.Label>Ort</Form.Label>
-                    <Form.Control type='text' />
+                    <Form.Control {...register('ort')} type='text' />
                 </Form.Group>
 
                 <Form.Group className='mb-3' controlId='beskrivning'>
                     <Form.Label>Beskrivning</Form.Label>
-                    <Form.Control type='text' />
+                    <Form.Control {...register('beskrivning')} type='text' />
                 </Form.Group>
 
                 <Form.Group className='mb-3' controlId='cuisine'>
                     <Form.Label>Cuisine</Form.Label>
-                    <Form.Control type='text' /> {/* TODO: change to select */}
+                    <Form.Control {...register('cuisine')} type='text' /> {/* TODO: change to select */}
                 </Form.Group>
                 
                 <Form.Group className='mb-3' controlId='typ'>
                     <Form.Label>Typ</Form.Label>
-                    <select className='form-select'>
+                    <select {...register('typ')} className='form-select'>
                         <option value='cafe'>Cafe</option>
                         <option value='restaurang'>Restaurang</option>
                         <option value='snabbmat'>Snabbmat</option>
@@ -46,7 +51,7 @@ const CreateNewPlacePage = () => {
 
                 <Form.Group className='mb-3' controlId='utbud'>
                     <Form.Label>Utbud</Form.Label>
-                    <select className='form-select'>
+                    <select {...register('utbud')} className='form-select'>
                         <option value='lunch'>Lunch</option>
                         <option value='after work'>After Work</option>
                         <option value='middag/a la carte'>Middag/A la Carte</option>
