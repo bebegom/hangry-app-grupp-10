27f4062838028369ae17fd73a2a4b1f14ea64976
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-09-22 11:46:42 +0200

    got all inputs requiered inputs on the page

--- src/pages/CreateNewPlacePage.jsx
+++ src/pages/CreateNewPlacePage.jsx
@@ -8,10 +8,51 @@ const CreateNewPlacePage = () => {
         <Container>
             <h1>Create a new place for the hangry</h1>
             <Form noValidate>
-                <Form.Group>
-                    <Form.Label>Name</Form.Label>
+                <Form.Group className='mb-3' controlId='namn'>
+                    <Form.Label>Namn</Form.Label>
                     <Form.Control type='text' />
                 </Form.Group>
+
+                <Form.Group className='mb-3' controlId='adress'>
+                    <Form.Label>Adress</Form.Label>
+                    <Form.Control type='text' />
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId='ort'>
+                    <Form.Label>Ort</Form.Label>
+                    <Form.Control type='text' />
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId='beskrivning'>
+                    <Form.Label>Beskrivning</Form.Label>
+                    <Form.Control type='text' />
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId='cuisine'>
+                    <Form.Label>Cuisine</Form.Label>
+                    <Form.Control type='text' /> {/* TODO: change to select */}
+                </Form.Group>
+                
+                <Form.Group className='mb-3' controlId='typ'>
+                    <Form.Label>Typ</Form.Label>
+                    <select className='form-select'>
+                        <option value='cafe'>Cafe</option>
+                        <option value='restaurang'>Restaurang</option>
+                        <option value='snabbmat'>Snabbmat</option>
+                        <option value='kiosk/grill'>Kiosk/Grill</option>
+                        <option value='foodtruck'>Foodtruck</option>
+                    </select>
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId='utbud'>
+                    <Form.Label>Utbud</Form.Label>
+                    <select className='form-select'>
+                        <option value='lunch'>Lunch</option>
+                        <option value='after work'>After Work</option>
+                        <option value='middag/a la carte'>Middag/A la Carte</option>
+                    </select>
+                </Form.Group>
+
                 <Button type='submit'>Create</Button>
             </Form>
         </Container>
