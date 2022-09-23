504a9f0c9b2840374426ccc72e753da4d447b52c
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-09-23 09:27:32 +0200

    changed alert to danger and it now works better

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,61 +1,62 @@
 import React, { useRef, useState } from 'react'
-import { Alert, Button, Card, Col, Container, Form, Row  } from 'react-bootstrap'
+import { Alert, Button, Card, Container, Col, Form, Row } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import { useAuthContext } from '../contexts/AuthContext'
 
 const LoginForm = () => {
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const { login } = useAuthContext()
-    const navigate = useNavigate
-    
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        setError(null);
+	const emailRef = useRef()
+	const passwordRef = useRef()
+	const [error, setError] = useState(null)
+	const [loading, setLoading] = useState(false)
+	const { login } = useAuthContext()
+	const navigate = useNavigate()
 
-        // try to log in 
-        try {
-            setLoading(true)
-            await login(emailRef.current.value, passwordRef.current.value)
-            navigate('/')
-        } catch (err) {
-            setError(err.message)
-            setLoading(false)
-        }
-        
-    }
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		setError(null);
 
-    return (
-        <Container className="py-3 center-y">
-            <Row>
-                <Col md={{ span: 6, offset: 3 }}>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title className="mb-3 text-center">Log In</Card.Title>
+		// try to log in 
+		try {
+			setLoading(true)
+			await login(emailRef.current.value, passwordRef.current.value)
+			navigate('/')
 
-                            {error && (<Alert variant='warning'>{error}</Alert>)}
+		} catch (err) {
+			setError(err.message)
+			setLoading(false)
+		}
+	}
 
-                            <Form onSubmit={handleSubmit}>
-                                <Form.Group id="email" className="mb-3">
-                                    <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" ref={emailRef} required />
-                                </Form.Group>
+	return (
+		<Container className="py-3 center-y">
+			<Row>
+				<Col md={{ span: 6, offset: 3 }}>
+					<Card>
+						<Card.Body>
+							<Card.Title className="mb-3">Log In</Card.Title>
 
-                                <Form.Group id="password" className="mb-3">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" ref={passwordRef} required />
-                                </Form.Group>
+							{error && (<Alert variant="danger">{error}</Alert>)}
 
-                                <Button disabled={loading} type="submit">Log In</Button>
-                            </Form>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    )
+							<Form onSubmit={handleSubmit}>
+
+								<Form.Group id="email" className="mb-3">
+									<Form.Label>Email</Form.Label>
+									<Form.Control type="email" ref={emailRef} required />
+								</Form.Group>
+
+								<Form.Group id="password" className="mb-3">
+									<Form.Label>Password</Form.Label>
+									<Form.Control type="password" ref={passwordRef} required />
+								</Form.Group>
+
+								<Button disabled={loading} type="submit">Log In</Button>
+							</Form>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	)
 }
 
 export default LoginForm
