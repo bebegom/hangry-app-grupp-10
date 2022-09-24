b8dfe9588b9e8be6f08c2250467583c2fff2c1b5
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-09-24 10:48:09 +0200

    ListGourp.Item is no longer a link, added a button that does nothing at the moment

--- src/components/TipsList.jsx
+++ src/components/TipsList.jsx
@@ -1,12 +1,13 @@
-import {ListGroup} from 'react-bootstrap'
+import {Button, ListGroup} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const TipsList = ({tips}) => {
     return (
     <ListGroup>
         {tips.map(tip => (
-            <ListGroup.Item action as={Link} to={`/tips/${tip.id}`} key={tip.id}>
+            <ListGroup.Item key={tip.id}>
                 {tip.message}
+                <Button>läst/radera</Button>
             </ListGroup.Item>
         ))}
     </ListGroup>
