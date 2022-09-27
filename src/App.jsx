2248d8a47bfe56289346f5db3d0d9f39a2b4faed
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-09-27 17:01:50 +0200

    added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from './components/Navigation'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LogInPage'
 import LogoutPage from './pages/LogoutPage'
+import SignupPage from './pages/SignupPage'
 import NotFound from './pages/NotFound'
 import CreateNewRestaurantPage from './pages/CreateNewRestaurantPage'
 import UpdateRestaurantPage from './pages/UpdateRestaurantPage'
@@ -18,6 +19,7 @@ function App() {
       <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/logout" element={<LogoutPage/>} />
+        <Route path="/signup" element={<SignupPage/>} />
       <Route path="*" element={<NotFound/>} />
       <Route path="/create-new-restaurant" element={<CreateNewRestaurantPage/>} />
       <Route path="/update-restaurant" element={<UpdateRestaurantPage/>} />
