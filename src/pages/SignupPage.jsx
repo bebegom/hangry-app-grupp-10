79ddbf7433c95484fd5d16fc0d7f1ed7b2b94e26
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-09-27 17:01:26 +0200

    added signup page

---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignupForm from '../components/SignupForm'
+
+const SignupPage = () => {
+    return (
+        <SignupForm />
+    )
+}
+
+export default SignupPage
