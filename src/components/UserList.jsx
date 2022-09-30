5c23c3b7b3ba136c8bea5c5137d83374405f53f0
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-09-30 17:26:34 +0200

    can now see list of all admins not users

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,6 +1,8 @@
 import Image from 'react-bootstrap/Image'
 
 const UserList = ({ users }) => {
+	const admin = users.filter(i => i.admin === true)
+
 	return (
 		<div>
 			<table className="table w-full">
@@ -14,13 +16,13 @@ const UserList = ({ users }) => {
 				</thead>
                 
 				<tbody>
-					{users &&
-						users.map((allUsers, i) => (
+					{admin &&  
+						admin.map((allUsers, i) => (
 							<tr key={allUsers.id}>
 								<th>{i + 1}</th>
 								<td>{allUsers.id}</td>
 								<td>{allUsers.email}</td>
-								<td><Image src={allUsers.photoURL} height={50} width={50} fluid roundedCircle /></td>
+								<td>{allUsers.photoURL ? <Image src={allUsers.photoURL} height={50} width={50} fluid roundedCircle /> : "No picture for you" } </td>
 							</tr>
 						))}
 				</tbody>
