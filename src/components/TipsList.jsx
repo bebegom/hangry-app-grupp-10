cd955b3cae5fcb184d13f1fe65aae965315f320d
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-10-03 13:44:57 +0200

    added button to link to page for creating new restaurants from tips

--- src/components/TipsList.jsx
+++ src/components/TipsList.jsx
@@ -1,16 +1,21 @@
 import {Button, ListGroup} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-const TipsList = ({tips}) => {
+const TipsList = ({ tips }) => {
     return (
-    <ListGroup>
-        {tips.map(tip => (
-            <ListGroup.Item key={tip.id}>
-                {tip.message}
-                <Button>läst/radera</Button>
-            </ListGroup.Item>
-        ))}
-    </ListGroup>
+        <ListGroup>
+            {tips.map(tip => (
+                <ListGroup.Item key={tip.id}>
+                    {tip.message}
+                    <Button className="mt-2"
+                        as={Link}
+                        to="/create-new-restaurant"
+                    >Create</Button>
+                    <Button className="mt-2"
+                    >Delete</Button>
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
     )
 }
 
