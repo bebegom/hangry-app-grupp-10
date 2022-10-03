66b4c647682232e79dc4e6a95c23cacf45079f9d
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-10-03 19:12:23 +0200

    fixed react table with restaurants

--- src/pages/RestaurantsListPage.jsx
+++ src/pages/RestaurantsListPage.jsx
@@ -1,16 +1,37 @@
 import { Container } from 'react-bootstrap'
+import { useMemo } from 'react'
 import useRestaurants from '../hooks/useRestaurants'
-import RestaurantsList from '../components/RestaurantsList'
+import SortableTable from '../components/SortableTable'
 
 const RestaurantsListPage = () => {
-    const { data: restaurants } = useRestaurants('restaurants')
+    const { data: restaurants, error, isError, isLoading } = useRestaurants('restaurants')
+
+    const columns = useMemo(() => {
+        return [
+            {
+                Header: 'ID',
+                accessor: 'id'
+            },
+            {
+                Header: 'Namn',
+                accessor: 'namn'
+            },
+        ]
+    }, [])
+
 
     return (
-        <Container>
-            <h1>Alla restauranger</h1>
+        <>
+            <Container>
+                <h1>Alla restauranger</h1>
 
-            <RestaurantsList restaurants={restaurants} />
+                {isLoading && (<p>Loading...</p>)}
+
+                {isError && (<p>{error.message}</p>)}
+
+                {restaurants && <SortableTable columns={columns} data={restaurants} />}
         </Container>
+        </>
     )
 }
 
