abb961773b1b637bc5b0ca25c58ef1a4b4852552
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-10-03 19:36:32 +0200

    added function to delete tips

--- src/components/TipsList.jsx
+++ src/components/TipsList.jsx
@@ -1,7 +1,16 @@
 import {Button, ListGroup} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { doc, deleteDoc } from 'firebase/firestore'
+import { db } from '../firebase'
 
 const TipsList = ({ tips }) => {
+    const handleDelete = async tip => {
+        console.log(tip)
+        
+        const ref = doc(db, 'tips', tip.id)
+        await deleteDoc(ref)
+    }
+
     return (
         <ListGroup>
             {tips.map(tip => (
@@ -11,7 +20,7 @@ const TipsList = ({ tips }) => {
                         as={Link}
                         to="/create-new-restaurant"
                     >Create</Button>
-                    <Button className="mt-2"
+                    <Button onClick={() => handleDelete(tip)} className="mt-2"
                     >Delete</Button>
                 </ListGroup.Item>
             ))}
