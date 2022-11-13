aab808e3be74d761da940e5c0ff17d5b92501db7
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-11-13 11:23:35 +0100

    using orderBy("namn") when getting the list of restaurants

--- src/hooks/useRestaurants.js
+++ src/hooks/useRestaurants.js
@@ -1,7 +1,8 @@
+import { orderBy } from "firebase/firestore"
 import useStreamCollection from "./useStreamCollection"
 
 const useRestaurants = () => {
-    return useStreamCollection('restaurants')
+    return useStreamCollection('restaurants', orderBy("namn"))
 }
 
 export default useRestaurants
